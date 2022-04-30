10d2e16f6bdf3fc73bc20d4626fa6ab2d0dceb71
yogeshpawar1958 <76243296+yogeshpawar1958@users.noreply.github.com>
2022-04-30 16:46:12 +0530

    Update secondlargest.js

--- secondlargest.js
+++ secondlargest.js
@@ -1,4 +1,4 @@
-var arr=[23,21,11,6,5,90,65,2,91];
+var arr=[23,21,11,6,5,90,65,2,91,91];
 var max=Number.MIN_VALUE;
 var second=Number.MIN_VALUE;
 
@@ -6,7 +6,7 @@ for(i=0;i<arr.length;i++){
     if(arr[i]>max){
         second=max;
         max=arr[i];
-    }else if(arr[i]>second){
+    }else if(arr[i]>second && arr[i]!==max){
        
         
     second=arr[i]
