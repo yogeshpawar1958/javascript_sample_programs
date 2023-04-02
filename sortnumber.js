d0fa1ba7c1b1f3fe5de611d25127b5b4a964b1f3
yogeshpawar1958 <76243296+yogeshpawar1958@users.noreply.github.com>
2023-04-02 22:06:18 +0530

    Update sortnumber.js

--- sortnumber.js
+++ sortnumber.js
@@ -3,3 +3,18 @@ var b=a.sort((a,b)=>{
    return a-b; //for descending order b-a
 })
 console.log(b)
+
+
+//usingbubble sort 
+const arr=[1,22,12,45,32,65,33]
+
+for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+    if(arr[i]>arr[j]){
+        let temp=arr[i]
+        arr[i]=arr[j]
+        arr[j]=temp
+    }
+}
+}
+console.log(arr)
